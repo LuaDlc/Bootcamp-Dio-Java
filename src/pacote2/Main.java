8af9cbceb1410a10fd21b01668e85d9154ea3493
LuaDlc <luadiaslc@gmail.com>
2022-05-18 16:36:24 -0300

    alterei para retornos

--- src/pacote2/Main.java
+++ src/pacote2/Main.java
@@ -5,11 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         //Quadrilatero
-        System.out.println("Exercicio quadrilatero");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5d, 5d);
-        Quadrilatero.area(7,8,9);
-        Quadrilatero.area(5f,5f);
+        //retornos
+        System.out.println("Exercicio quadrilatero e retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Area do quadrado:" + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5,5);
+        System.out.println("Area do retangulo" + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Area do trapezio:" + areaTrapezio);
+
         
     }
     
